b8868a173d00ae3d1fcb3d3d782e49350740c3b4
A1 <internalmike@gmail.com>
2017-12-07 13:16:27 +0300

    fix haddock warning

--- src/Control/Error/Extensions.hs
+++ src/Control/Error/Extensions.hs
@@ -28,7 +28,8 @@ import Control.Monad.Catch
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Except
 
--- | Analogous to 'Control.Exception.bracket', but for 'ExceptT' over 'IO' (or any 'MonadMask' monad).
+-- | Analogous to 'Control.Exception.bracket', but for 'ExceptT' over 'IO'
+-- (or 'ExceptT' over any 'Control.Monad.Catch.MonadMask' monad).
 bracketE :: MonadMask m => ExceptT e m a -> (a -> ExceptT e m b) -> (a -> ExceptT e m c) -> ExceptT e m c
 bracketE acquire release action = (hoistEither =<<) $ lift $ do
   resource <- runExceptT acquire
